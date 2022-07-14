0233e6c447027f6f414e303ed1469ad4e937fbe8
deepakpatel1999 <deeppatelg1122@gmail.com>
2022-07-14 19:11:28 +0530

    E-SHOPER

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -7,19 +7,19 @@ use Illuminate\Http\Request;
 
 class IsAdmin
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-     */
-    public function handle(Request $request, Closure $next)
-    {
-       if(auth()->user()->is_admin == 1){
-            return $next($request);
-        }
-   
-        return redirect(‘home’)->with(‘error’,"You don't have admin access.");
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+   * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+   */
+  public function handle(Request $request, Closure $next)
+  {
+    if (auth()->user()->is_admin == 1) {
+      return $next($request);
     }
+
+    return redirect(‘home’)->with(‘error’, "You don't have admin access.");
+  }
 }
--- resources/views/admin/sitebar.blade.php
+++ resources/views/admin/sitebar.blade.php
@@ -91,6 +91,15 @@
                  </ul>
 
              </li>
+
+             <li><a><i class="fa fa-edit"></i> E-SHOPPER <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+
+                    <li><a href="{{ route('setting-display') }}">E-Shoper Banner </a></li>
+
+                </ul>
+
+            </li>
      </div>
 
 
